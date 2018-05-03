a0859e7199e32dd8589c21c622503cdc353ef86f
岳天宇 <yuetianyu2016@163.com>
2018-05-03 18:36:29 +0800

    用webpack4 快速搭建开发san helloworld

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import san from 'san'
+
+import {router} from 'san-router'
+import San from './app.san'
+
+if(process.env.NODE_ENV !== 'production') {
+	console.log('Looks like we are in development mode!')
+}
+
+console.log('hello webpack San')
+
+router.add({
+	rule:'/',
+	Component:San,
+	target:'#app'
+})
+
+
+router.start()
+
